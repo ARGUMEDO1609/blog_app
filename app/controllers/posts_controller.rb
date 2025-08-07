2cc3c882849786eb30fb5273782eecad032f60a1
argumedo1609 <noslenque931@gmail.com>
2025-08-07 14:59:39 -0500

    Agregar paginación a publicaciones con Kaminari

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(5) 
   end
 
   def search
